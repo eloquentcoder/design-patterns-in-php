b595a90defa4951cea67bd9e83cd1ff296861a50
eloquentcoder <eloquentintech@gmail.com>
2022-08-07 17:49:14 +0100

    The Expert Pattern PHP

---
+++ GraspRules/ExpertPattern.php
@@ -0,0 +1,7 @@
+<?php
+
+// if an object A is going to instantiate B, it must have 
+// all the information needed to instantiate the object B.
+// Object A must be an expert on object B. We can use the 
+// same example in the creator pattern. The bike class is an 
+// expert of how to class wheel and frame
